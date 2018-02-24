5ddb222f45222367d96b20a3b2d0628ccd3dbbcf
andrewnc <andrewcarr06@gmail.com>
2018-02-24 15:36:18 -0700

    frozen code

--- terms-bot/src/page_action/page_action.js
+++ terms-bot/src/page_action/page_action.js
@@ -1,5 +1,8 @@
 chrome.tabs.query({'active': true, 'currentWindow': true}, function (tab) {
     chrome.tabs.sendMessage(tab[0].id, {tag: "from_popup"}, function (response) {
-        document.body.innerHTML = response.data;
+    	document.getElementById("summary_data").innerHTML = response.data;
     });
 });
+
+
+
