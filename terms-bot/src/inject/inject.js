18226e09a4fa8125cb7bddd029ced7aad15baafc
Joshua Greaves <joshua.greaves@gmail.com>
2018-03-10 21:05:52 -0700

    Refactored AppEngine code and made page action a browser action

--- terms-bot/src/inject/inject.js
+++ terms-bot/src/inject/inject.js
@@ -1,25 +1,30 @@
-var body_text = window.location.href;
-chrome.runtime.sendMessage({
-    status: 'sending',
-    data: body_text,
-    url: window.location.hostname
-}, function (response) {
-    var readyStateCheckInterval = setInterval(function () {
-        if (document.readyState === "complete") {
-            clearInterval(readyStateCheckInterval);
-        }
-    }, 30);
-});
+var words = new Set(['terms', 'service', 'legal', 'agreement']);
+
+function getSummary() {
+    chrome.runtime.sendMessage({
+        tag: 'request',
+        data: document.body.innerHTML,
+        url: window.location.href
+    }, function (response) {
+        var readyStateCheckInterval = setInterval(function () {
+            if (document.readyState === "complete") {
+                clearInterval(readyStateCheckInterval);
+            }
+        }, 30);
+    });
+}
 
 var info = {};
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.tag === "from_background") {
-            info = {
-                data: request.resp
-            };
-            sendResponse(info);
+            chrome.runtime.sendMessage({
+                tag: 'for_popup',
+                data: request.resp,
+            }, function (response) {
+                // Do nothing
+            });
         } else if (request.tag === "from_popup") {
-            sendResponse(info);
+            getSummary();
         }
     });
